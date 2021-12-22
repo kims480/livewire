be926f4a8f8981c723e331762434e1ada3d6c2f0
kims480 <eng.karim2010@gmail.com>
2021-12-23 00:42:46 +0400

    Installing Laravel $ Livewire

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h1>Livewire</h1>
+    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <div style="text-align: center">
+        <button wire:click="increment">+</button>
+        <h1>{{ $count }}</h1>
+    </div>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use JeroenNoten\LaravelAdminLte\Http\Controllers\DarkModeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your package. These
+| routes are loaded by your package ServiceProvider within a group which
+| contains the "web" middleware group.
+|
+*/
+
+//-----------------------------------------------------------------------------
+// Dark Mode routes.
+//-----------------------------------------------------------------------------
+// Route::get('/',function(){
+//     echo "Laravel";
+// });
+Route::post('/darkmode/toggle', [DarkModeController::class, 'toggle'])
+    ->name('darkmode.toggle');
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
